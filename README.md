9843b6fe288ace238691e232b47b9ad19c2ba9bb
Colum Bennett <colum.bennett@feedhenry.com>
2015-10-11 14:37:51 +0100

    prep for publish

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ![Build Passing](https://travis-ci.org/col1985/exists.svg?branch=master)
 [![NPM version](https://badge.fury.io/js/exists.svg)](http://badge.fury.io/js/exists)
 
+
---
+++ index.js
@@ -0,0 +1,54 @@
+var _ = require('underscore');
+
+/**
+ * exists
+ * @param  {object} d datatype (array or string)
+ * @param  {string} p param
+ * @return {boolean}
+ */
+function exists(d, p) {
+  if (isVaild(d) && isVaild(p)) {
+    return check(d, p);
+  } else {
+    console.error('ERROR: exists: Invaild dataType');
+    return false;
+  }
+}
+
+/**
+ * isVaild
+ * @param  {object}  d datatype (object, function, string, array)
+ * @return {Boolean} if vaild datatype
+ */
+function isVaild(d) {
+  var isArray = (_.isObject(d) && _.isArray(d)),
+    isString = _.isString(d),
+    isFunction = _.isFunction(d),
+    isObject = _.isObject(d) && (!isFunction || !isArray);
+
+  return (isArray || isObject ||
+    isFunction || isString);
+}
+
+/**
+ * check
+ * @param  {object} d
+ * @param  {string} p
+ * @return {boolean}
+ */
+function check(d, p) {
+  var isArray = _.isArray(d),
+    isString = _.isString(d),
+    isObject = _.isObject(d),
+    isFunction = _.isFunction(d);
+
+  if (isObject && (!isFunction && !isArray)) {
+    return _.has(d, p);
+  } else if (isArray || isString) {
+    return _.indexOf(d, p) !== -1;
+  } else {
+    return false;
+  }
+}
+
+module.exports = exists;
